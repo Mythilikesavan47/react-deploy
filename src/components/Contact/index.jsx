8a82370a8911587d36f8133cad304d3921b9e3ab
Mythili <mythilik2006@gmail.com>
2025-01-02 15:46:37 +0530

    modified home page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './contact.css';
 import Footer from '../Footer';
 
-
+const Contact = () => {
+  return (
+    <div className="home-container">
+      <div className="welcome-text">
+        <h1>Contact</h1>
+        <p>Your one-stop solution for everything you need edited.</p>
+      </div>
       <Footer />
 
     </div>
