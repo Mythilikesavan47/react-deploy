6532611cbbfea69f6238f83979a35bd39a7a07d4
Mythili <mythilik2006@gmail.com>
2025-01-05 13:04:41 +0530

    change navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './navbar.css'; 
+import './Navbar.css'; 
 import logo from '../../assets/images/star-bucks.png';
 
 const Navbar = () => {
